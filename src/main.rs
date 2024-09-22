82823b1270accc4bf36c21382bdb5e3b7363b57f
Saheed Adeleye <aadesaed@gmail.com>
2024-09-22 14:43:23 +0100

    feat2: more improvements to the parsing logic
    - parse multiple Telegrams
    - added more testing logic
    - detect illegal formations
    - exit with code 42 on rejection of a Telegram

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use error::MainError;
 use std::io::Read;
 use std::str;
-use tudelft_dsmr_output_generator::current_over_time::CurrentData;
-use tudelft_dsmr_output_generator::date_to_timestamp;
 use tudelft_dsmr_output_generator::voltage_over_time::{
     create_voltage_over_time_graph, VoltageData,
 };
@@ -68,9 +66,15 @@ struct Electricity {
 }
 
 #[derive(Clone, Debug)]
+enum Severity {
+    Low,
+    High,
+}
+
+#[derive(Clone, Debug)]
 struct EventLog {
     id: u32,
-    severity: char, // should be bool?
+    severities: Vec<Severity>, // should be String?
     date: i64,
     message: String,
 }
@@ -81,6 +85,7 @@ pub enum ParseError {
     NoDate,
     DuplicateFieldId,
     MissingElectricity,
+    ChildTelegramNotSupported,
 }
 
 /// Parse v10 of DSMR spec
@@ -103,7 +108,7 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
 
     let mut event_log = EventLog {
         id: 0,
-        severity: '\0',
+        severities: vec![],
         date: -1,
         message: String::new(),
     };
@@ -120,21 +125,41 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
         match bytes[0] {
             b'1' => {
                 // parse telegram header
+
+                if bytes[2] == b'1' && bytes[4] != b'0' {
+                    return Err(ParseError::ChildTelegramNotSupported);
+                }
                 if bytes[2] == b'2' {
                     // telegram end
                     if !has_electricity {
                         return Err(ParseError::MissingElectricity);
                     }
+                    if !has_telegram_date {
+                        return Err(ParseError::NoDate);
+                    }
+
                     // push it to list
                     dsmr.push(TelegramV10 {
                         event_log: event_log.clone(),
                         information: electricity.clone(),
                     });
-                } else {
-                    seen_info_type = false;
-                    has_electricity = false;
-                    has_telegram_date = false;
                 }
+
+                // new telegram
+                seen_info_type = false;
+                has_electricity = false;
+                has_telegram_date = false;
+
+                event_log.id = 0;
+                event_log.date = -1;
+                event_log.severities.clear();
+                event_log.message.clear();
+
+                electricity.power = 0.0;
+                electricity.voltage = 0.0;
+                electricity.current = 0.0;
+                electricity.total_consumed = 0.0;
+                electricity.total_produced = 0.0;
             }
             b'2' => {
                 let idx = line.rfind(')').unwrap();
@@ -145,27 +170,40 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
             b'3' => {
                 // parse eventlog
 
-                let event_id = bytes[4] as char;
-                let disc_n = bytes[2] as char;
+                let discriminant = bytes[2] as char;
                 let paren = line.rfind(')').unwrap();
-
                 let val = &bytes[7..paren];
+
+                let event_id = bytes[4] as char;
                 event_log.id = event_id.to_digit(10).unwrap();
-                match disc_n {
-                    '1' => event_log.severity = bytes[7] as char,
+                match discriminant {
+                    '1' => {
+                        event_log
+                            .severities
+                            .push(if matches!(bytes[7] as char, 'H') {
+                                Severity::High
+                            } else {
+                                Severity::Low
+                            });
+                    }
                     '2' => event_log.message = String::from_utf8_lossy(&val).to_string(),
                     '3' => {
                         // parse date
+
+                        // <yy-mmm-dd hh:mm:ss>
+                        let date = String::from_utf8_lossy(&val[..val.len() - 4]).to_string();
+
                         let dts = val[val.len() - 2] as char;
-                        let date = String::from_utf8_lossy(&val[..val.len() - 4]).to_string(); // <yy-mmm-dd hh:mm:ss>
-                        let yy: u16 = (&date[0..2]).parse().unwrap();
+                        let yy = 2000 /* account for this century */ + (&date[0..2]).parse::<u16>().unwrap();
                         let dd: u8 = (&date[7..9]).parse().unwrap();
                         let hh: u8 = (&date[10..12]).parse().unwrap();
                         let mm: u8 = (&date[13..15]).parse().unwrap();
                         let ss: u8 = (&date[16..18]).parse().unwrap();
                         let mmm: u8 = get_month_as_uint(&date[3..6]);
+
                         println!("data: {yy}-{mmm}-{dd} {hh}:{mm}:{ss}");
-                        let timestamp: i64 = tudelft_dsmr_output_generator::date_to_timestamp(
+
+                        event_log.date = tudelft_dsmr_output_generator::date_to_timestamp(
                             yy,
                             mmm,
                             dd,
@@ -175,16 +213,12 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
                             dts == 'S',
                         )
                         .unwrap();
-                        event_log.date = timestamp;
                     }
                     _ => unreachable!(),
                 }
             }
             b'4' => {
                 // parse informtion type
-                if !has_telegram_date {
-                    return Err(ParseError::NoDate);
-                }
 
                 if seen_info_type {
                     return Err(ParseError::DuplicateFieldId);
@@ -200,25 +234,41 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
                 }
                 has_electricity = true;
 
-                let disc_n = bytes[2] as char;
+                let phase = match bytes[4] as char {
+                    '1' => Phase::Phase1,
+                    '2' => Phase::Phase2,
+                    '3' => Phase::Phase3,
+                    _ => unreachable!(),
+                };
+                let discriminant = bytes[2] as char;
                 let star = line.find('*').unwrap();
                 let val = str::from_utf8(&bytes[7..star]).unwrap();
                 let val_f64 = val.parse::<f64>().unwrap();
-                if '1' <= disc_n && disc_n <= '3' {
-                    match disc_n {
-                        '1' => {}
-                        '2' => {}
+
+                if '1' <= discriminant && discriminant <= '4' {
+                    match discriminant {
+                        '1' => {
+                            electricity.voltage_phase = phase;
+                            electricity.voltage = val_f64.max(electricity.voltage);
+                        }
+                        '2' => {
+                            electricity.current = val_f64.max(electricity.current);
+                            electricity.current_phase = phase;
+                        }
                         '3' => {
-                            // electricity.power += val_f64;
+                            electricity.power += val_f64;
+                            electricity.power_phase = phase;
+                        }
+                        '4' => {
+                            if bytes[4] == b'1' {
+                                electricity.total_consumed += val_f64;
+                            } else {
+                                electricity.total_produced += val_f64;
+                            }
                         }
                         _ => unreachable!(),
                     }
                 } else {
-                    // if bytes[4] == b'1' {
-                    //     electricity.total_consumed += val_f64;
-                    // } else {
-                    //     electricity.total_produced += val_f64;
-                    // }
                 }
             }
             _ => {}
@@ -235,12 +285,10 @@ fn parse(_input: &str) -> Result<DsmrV10, ParseError> {
     // tudelft_dsmr_output_generator::date_to_timestamp(4);
     let version = &_input[1..4];
     if version == "v10" {
-        parse_v10(_input)?;
+        return parse_v10(_input);
     } else {
         return Err(ParseError::UnknownTelegramVersion);
     }
-
-    Ok(vec![])
 }
 
 #[test]
@@ -261,8 +309,14 @@ pub fn fail_on_missing_date() {
 
 #[test]
 pub fn fail_on_v12() {
-    let input = std::fs::read_to_string("examples/good/simple_gas.dsmr").unwrap();
-    assert!(parse(&input).is_err());
+    for i in 0..4 {
+        let fp_rec = std::fmt::format(format_args!(
+            "examples/good_sequences/should_parse_{i}_recursive.dsmr"
+        ));
+
+        let input = std::fs::read_to_string(fp_rec).unwrap();
+        assert!(parse(&input).is_err());
+    }
 }
 
 #[test]
@@ -272,6 +326,17 @@ pub fn can_parse_multiple_telegrams() {
     println!("{maybe_dsmr:#?}");
     assert!(maybe_dsmr.is_ok());
     assert_eq!(maybe_dsmr.unwrap().len(), 2);
+
+    for i in 0..4 {
+        let fp = std::fmt::format(format_args!(
+            "examples/good_sequences/should_parse_{i}.dsmr"
+        ));
+        let input = std::fs::read_to_string(fp).unwrap();
+        let count = input.matches("(END)").count();
+        let maybe_dsmr = parse(&input);
+        assert!(maybe_dsmr.is_ok());
+        assert_eq!(maybe_dsmr.unwrap().len(), count);
+    }
 }
 
 /// Reads the DSMR file from the terminal.
@@ -293,7 +358,9 @@ fn read_from_stdin() -> Result<String, MainError> {
 fn main() -> Result<(), MainError> {
     let input = read_from_stdin()?;
 
-    let _parsed = parse(&input).unwrap();
+    let parsed = parse(&input).unwrap_or_else(|_| std::process::exit(42));
+
+    println!("parsed: {parsed:#?}");
 
     let mut result = Graphs::new()?;
 
