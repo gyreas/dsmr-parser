effbad9194ead9ca040c568992968a6757172bba
Saheed Adeleye <aadesaed@gmail.com>
2024-09-23 06:06:52 +0100

    feat4: implement plotting voltage and current over time along with their event log messages

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use error::MainError;
 use std::io::Read;
 use std::str;
-use tudelft_dsmr_output_generator::voltage_over_time::{
-    create_voltage_over_time_graph, VoltageData,
-};
 use tudelft_dsmr_output_generator::Graphs;
+use tudelft_dsmr_output_generator::{
+    current_over_time::{CurrentData, CurrentOverTime},
+    voltage_over_time::{create_voltage_over_time_graph, VoltageData},
+    GraphBuilder,
+};
 
 /// Contains `MainError`, and code to convert `PlotError` and `io::Error` into a `MainError`
 mod error;
@@ -41,27 +43,18 @@ type DsmrV10 = Vec<TelegramV10>;
 
 #[derive(Debug)]
 struct TelegramV10 {
-    date: String,
+    date: i64,
     event_log: EventLog,
     information: Electricity,
 }
 
 #[derive(Clone, Debug)]
-enum Phase {
-    Phase1 = 1,
-    Phase2,
-    Phase3,
-}
-
-#[derive(Clone, Debug)]
 struct Electricity {
-    power_phase: Phase,
-    current_phase: Phase,
-    voltage_phase: Phase,
+    // [phase_1, phase_2, phase_3]
+    power: Vec<f64>,
+    voltage: Vec<f64>,
+    current: Vec<f64>,
 
-    power: f64,
-    voltage: f64,
-    current: f64,
     total_consumed: f64,
     total_produced: f64,
 }
@@ -96,19 +89,16 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
 
     let mut dsmr = DsmrV10::new();
     let mut electricity = Electricity {
-        current_phase: Phase::Phase1,
-        voltage_phase: Phase::Phase1,
-        power_phase: Phase::Phase1,
+        power: vec![0.0, 0.0, 0.0],
+        voltage: vec![0.0, 0.0, 0.0],
+        current: vec![0.0, 0.0, 0.0],
 
-        power: 0.0,
-        voltage: 0.0,
-        current: 0.0,
         total_consumed: 0.0,
         total_produced: 0.0,
     };
 
     // TODO: i64 should be UnixTimeStamp for clarity
-    let mut telegram_date = String::new();
+    let mut telegram_date = 0i64;
     let mut dates = Vec::new();
     let mut messages = Vec::new();
     let mut severities = Vec::new();
@@ -161,7 +151,7 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
                         messages: Vec::new(),
                         severities: Vec::new(),
                     };
-
+                    // avoiding iterators here because it won't be easy to follow
                     for i in 0..dates.len() {
                         let ent_date = dates[i];
                         let ent_sev = severities[i];
@@ -197,20 +187,42 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
                 has_electricity = false;
                 has_telegram_date = false;
 
-                telegram_date.clear();
+                telegram_date = 0;
                 dates.clear();
                 severities.clear();
                 messages.clear();
 
-                electricity.power = 0.0;
-                electricity.voltage = 0.0;
-                electricity.current = 0.0;
+                electricity.power = [0.0].repeat(3);
+                electricity.voltage = [0.0].repeat(3);
+                electricity.current = [0.0].repeat(3);
                 electricity.total_consumed = 0.0;
                 electricity.total_produced = 0.0;
             }
             b'2' => {
+                // parse this Telegram's date
+
                 let idx = line.rfind(')').unwrap();
-                telegram_date = String::from_utf8_lossy(&bytes[5..idx]).to_string();
+                let inner = &bytes[5..idx];
+                let date = String::from_utf8_lossy(inner).to_string();
+                let yy =
+                    2000 /* account for this century */ + (&date[0..2]).parse::<u16>().unwrap();
+                let dd: u8 = (&date[7..9]).parse().unwrap();
+                let hh: u8 = (&date[10..12]).parse().unwrap();
+                let mm: u8 = (&date[13..15]).parse().unwrap();
+                let ss: u8 = (&date[16..18]).parse().unwrap();
+                let mmm: u8 = get_month_as_uint(&date[3..6]);
+
+                let dts = inner[inner.len() - 2] as char;
+                telegram_date = tudelft_dsmr_output_generator::date_to_timestamp(
+                    yy,
+                    mmm,
+                    dd,
+                    hh,
+                    mm,
+                    ss,
+                    dts == 'S',
+                )
+                .unwrap();
                 has_telegram_date = true;
             }
             b'3' => {
@@ -266,11 +278,9 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
             }
             b'4' => {
                 // parse informtion type
-
                 if seen_info_type {
                     return Err(ParseError::DuplicateFieldId);
                 }
-
                 seen_info_type = true;
             }
             b'7' => {
@@ -280,41 +290,30 @@ fn parse_v10(input: &str) -> Result<DsmrV10, ParseError> {
                 }
                 has_electricity = true;
 
-                let phase = match bytes[4] as char {
-                    '1' => Phase::Phase1,
-                    '2' => Phase::Phase2,
-                    '3' => Phase::Phase3,
-                    _ => unreachable!(),
-                };
+                let phase = (bytes[4] - b'1') as usize; // get 0-based index for use in phases vectors
+                assert!(phase <= 2);
+
                 let discriminant = bytes[2] as char;
                 let star = line.find('*').unwrap();
                 let val = str::from_utf8(&bytes[7..star]).unwrap();
                 let val_f64 = val.parse::<f64>().unwrap();
 
-                if '1' <= discriminant && discriminant <= '4' {
-                    match discriminant {
-                        '1' => {
-                            electricity.voltage_phase = phase;
-                            electricity.voltage = val_f64.max(electricity.voltage);
+                // just in case, and for simplicity
+                if discriminant == '0' || discriminant > '4' {
+                    unreachable!();
+                }
+                match discriminant {
+                    '1' => electricity.voltage[phase] = val_f64,
+                    '2' => electricity.current[phase] = val_f64,
+                    '3' => electricity.power[phase] = val_f64,
+                    '4' => {
+                        if bytes[4] == b'1' {
+                            electricity.total_consumed = val_f64;
+                        } else {
+                            electricity.total_produced = val_f64;
                         }
-                        '2' => {
-                            electricity.current = val_f64.max(electricity.current);
-                            electricity.current_phase = phase;
-                        }
-                        '3' => {
-                            electricity.power += val_f64;
-                            electricity.power_phase = phase;
-                        }
-                        '4' => {
-                            if bytes[4] == b'1' {
-                                electricity.total_consumed += val_f64;
-                            } else {
-                                electricity.total_produced += val_f64;
-                            }
-                        }
-                        _ => unreachable!(),
                     }
-                } else {
+                    _ => unreachable!(),
                 }
             }
             _ => {}
@@ -332,6 +331,88 @@ fn parse(_input: &str) -> Result<DsmrV10, ParseError> {
     } else {
         return Err(ParseError::UnknownTelegramVersion);
     }
+}
+
+fn get_event_log_message(dsmr: &DsmrV10) -> Vec<Vec<(Severity, String)>> {
+    dsmr.iter()
+        .map(|tel: &TelegramV10| {
+            // each Telegram
+            tel.event_log
+                .severities
+                .iter()
+                .zip(tel.event_log.messages.iter())
+                .map(|(severity, msg): (&Severity, &String)| {
+                    // TODO: assert the length is even number
+                    (
+                        *severity,
+                        msg.as_bytes()
+                            .chunks_exact(2)
+                            .map(|dbytes| {
+                                // given msg: AABB..ZZ..
+                                // dbytes = [A, A]|[B, B]|..
+                                // A is a byte form the hex that represent the final char
+                                // h = A -> 1/2 character
+                                // l = A -> 1/2 character
+                                // convert to decimal and then to the final char
+                                let h = (dbytes[0] as char).to_digit(16).unwrap();
+                                let l = (dbytes[1] as char).to_digit(16).unwrap();
+                                let b = char::from_u32(h * 16 + l).unwrap();
+                                b
+                            })
+                            .collect::<String>(),
+                    )
+                })
+                .collect::<Vec<(Severity, String)>>()
+        })
+        .collect()
+}
+
+fn get_voltage_data(dsmr: &DsmrV10) -> Vec<VoltageData> {
+    dsmr.iter()
+        .map(|tel| {
+            // each Telegram
+            let timestamp = tel.date;
+            let phases = &tel.information.voltage[..];
+            let phase_1 = phases[0];
+            let phase_2 = phases[1];
+            let phase_3 = phases[2];
+
+            VoltageData {
+                phase_1,
+                phase_2,
+                phase_3,
+                timestamp,
+            }
+        })
+        .collect()
+}
+
+pub fn create_current_over_time_graph(data: Vec<CurrentData>) -> CurrentOverTime {
+    let mut cuovt = CurrentOverTime::new();
+    for cdata in data {
+        cuovt.add(cdata);
+    }
+    cuovt
+}
+
+fn get_current_data(dsmr: &DsmrV10) -> Vec<CurrentData> {
+    dsmr.iter()
+        .map(|tel| {
+            // each Telegram
+            let timestamp = tel.date;
+            let phases = &tel.information.current[..];
+            let phase_1 = phases[0];
+            let phase_2 = phases[1];
+            let phase_3 = phases[2];
+
+            CurrentData {
+                phase_1,
+                phase_2,
+                phase_3,
+                timestamp,
+            }
+        })
+        .collect()
 }
 
 #[test]
@@ -403,24 +484,26 @@ fn main() -> Result<(), MainError> {
 
     let parsed = parse(&input).unwrap_or_else(|_| std::process::exit(42));
 
-    println!("parsed: {parsed:#?}");
+    let voltage_data = get_voltage_data(&parsed);
+    let current_data = get_current_data(&parsed);
+    let evlog_msg = get_event_log_message(&parsed);
+
+    // println!("parsed: {parsed:#?}");
+    println!("voltage_data: {voltage_data:#?}");
+    println!("evlog_msg: {evlog_msg:#?}");
 
     let mut result = Graphs::new()?;
-
-    result.add_graph(create_voltage_over_time_graph(vec![
-        VoltageData {
-            phase_1: 100.0,
-            phase_2: 200.0,
-            phase_3: 300.0,
-            timestamp: 100,
-        },
-        VoltageData {
-            phase_1: 200.0,
-            phase_2: 300.0,
-            phase_3: 250.0,
-            timestamp: 10000,
-        },
-    ]))?;
+    for log_msgs in evlog_msg {
+        for (sev, msg) in log_msgs {
+            if matches!(sev, Severity::Low) {
+                result.add_low_severity_event_log_message(msg);
+            } else {
+                result.add_high_severity_event_log_message(msg);
+            }
+        }
+    }
+    result.add_graph(create_voltage_over_time_graph(voltage_data))?;
+    result.add_graph(create_current_over_time_graph(current_data))?;
     result.generate()?;
 
     Ok(())
